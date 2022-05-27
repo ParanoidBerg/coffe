0506fb3f14327939ae605daebf643eb1a8142c82
Paranoid <magomaevd99@gmail.com>
2022-05-27 10:11:46 +0300

    task completed

--- controllers/drinks.controller.js
+++ controllers/drinks.controller.js
@@ -3,7 +3,7 @@ const Drinks = require('../models/drinks.model')
 module.exports.drinksController = {
     getDrinks: (req, res) => {
         Drinks.find().select({name: req.body.name, price: req.body.price}).then((data)=> {res.json(data)})
-        .catch((err)=>{res.json('Ошибка')})
+        .catch((err)=>{res.json(err.message)})
     },
 
     postDrink: (req, res) => {
@@ -15,7 +15,7 @@ module.exports.drinksController = {
             size: req.body.size,
             about: req.body.about
 
-        }).then(()=>{res.json('Напиток добавлен')}).catch((err)=>{res.json('Ошибка')})
+        }).then(()=>{res.json('Напиток добавлен')}).catch((err)=>{res.json(err.message)})
     },
 
     patchDrink: (req, res) => {
@@ -27,15 +27,15 @@ module.exports.drinksController = {
                 isCoffeineFree: req.body.isCoffeineFree,
                 size: req.body.size,
                 about: req.body.about
-            }).then(()=>{res.json('Напиток изменен')}).catch((err)=>{res.json('Ошибка')})
+            }).then(()=>{res.json('Напиток изменен')}).catch((err)=>{res.json(err.message)})
     },
 
     deleteDrinks: (req, res) => {
-        Drinks.findByIdAndRemove(req.params.id).then(()=>(res.json('Напиток удален'))).catch((err)=>{res.json('Ошибка')})
+        Drinks.findByIdAndRemove(req.params.id).then(()=>(res.json('Напиток удален'))).catch((err)=>{res.json(err.message)})
     },
 
     getDrinksById: (req, res) => {
-        Drinks.findById(req.params.id).then((data)=>{res.json(data)}).catch((err)=>{res.json('Ошибка')})
+        Drinks.findById(req.params.id).then((data)=>{res.json(data)}).catch((err)=>{res.json(err.message)})
     },
     getAvailableDrinks: (req, res) => {
         Drinks.find({isAvailable: true }).then((data)=>{res.json(data)}).catch((err)=>{res.json(err.message)})
